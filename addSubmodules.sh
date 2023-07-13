b1fe0b9847744abfb70fa0d3db8f24be4116c0af
xwasoux <oxwasouxo@gmail.com>
2023-07-14 02:24:48 +0900

    Add: Some modules for tree-sitter

---
+++ addSubmodules.sh
@@ -0,0 +1,8 @@
+cd ./astar/parser
+mkdir grammar grammar/tree-sitter 
+
+git submodule add https://github.com/tree-sitter/tree-sitter-c.git grammar/tree-sitter/tree-sitter-c
+git submodule add https://github.com/tree-sitter/tree-sitter-cpp.git grammar/tree-sitter/tree-sitter-cpp
+git submodule add https://github.com/tree-sitter/tree-sitter-python.git grammar/tree-sitter/tree-sitter-python
+
+cd ../..
