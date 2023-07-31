562d87e16b0f17ab3f1715d828369647f5b0e598
xwasoux <oxwasouxo@gmail.com>
2023-07-31 17:36:39 +0900

    Add: Deletion Methods

--- astars/analyser/analyser.py
+++ astars/analyser/analyser.py
@@ -1,10 +1,14 @@
 import logging
 import anytree
+from copy import deepcopy
 from anytree import RenderTree
 
 from ..nodes import ANode
 from .traverse import AllNodeTraverser
 from .operator import AstOperator
+from .traverse import ATypeTraverser
+from .traverse import AIDTraverser
+from .searcher import ASearcher
 
 class AstAnalyser:
 
@@ -23,6 +27,54 @@ class AstAnalyser:
         _print(tree, attr)
         return None
     
+    @staticmethod
+    def forwardSequencialCodeDelete(tree) -> list:
+        return _sequencialCodeDlete(tree, False)
+
+    @staticmethod
+    def backwardSequencialCodeDelete(tree) -> list:
+        return _sequencialCodeDlete(tree, True)
+
+    @staticmethod
+    def pointingCodeDelete(tree) -> list:
+        return _pointingCodeDelete(tree)
+
+    @staticmethod
+    def selectedPointingCodeDelete(tree, types:list) -> list:
+        ids = [node.id for node in ATypeTraverser.leftPostOrder(tree) if node.type in types]
+        return _pointingCodeDelete(tree, ids)
+
+    
+    
+def _sequencialCodeDlete(tree:ANode, reversal:bool) -> list:
+    res = []
+    dupTree = deepcopy(tree)
+
+    if not reversal:
+        allNodeList = AllNodeTraverser.leftPostOrder(dupTree)
+    elif reversal:
+        allNodeList = AllNodeTraverser.rightPostOrder(dupTree)
+    
+    for subtree in allNodeList:
+        editedTree = AstOperator.delete(root=dupTree, target=subtree)
+        res.append(deepcopy(editedTree))
+    return res
+
+def _pointingCodeDelete(tree:ANode, selections:list=None) -> list:
+    res = []
+
+    if selections == None:
+        selections = AIDTraverser.leftPostOrder(tree)
+    
+    for nodeId in selections:
+        dupTree = deepcopy(tree)
+        targetNode = ASearcher.searchNode(dupTree, str(nodeId))
+
+        editedTree = AstOperator.delete(root=dupTree, target=targetNode)
+        res.append(editedTree)
+
+    return res
+
 def _print(tree:ANode, attr:str) -> None:
     logging.basicConfig(format='%(message)s',
                         level=logging.INFO)
