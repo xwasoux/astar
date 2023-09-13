05f0dce37108caebb081929b35a3af0c5e79e74e
xwasoux <oxwasouxo@gmail.com>
2023-09-13 12:06:37 +0900

    refactor: unified them to AParseTree class

--- astars/tree/tree.py
+++ astars/tree/tree.py
@@ -1,6 +1,8 @@
 from anytree import RenderTree
+from anytree import find, find_by_attr, findall, findall_by_attr
 
 from ..nodes import ANode
+from ._updater import _textDelete, _generate
 
 class AParseTree:
     def __init__(self, tree:ANode, code:str, lang:str) -> None:
@@ -14,10 +16,36 @@ class AParseTree:
     def __str__(self) -> str:
         return RenderTree(self.root).by_attr("type")
 
-    def textUpdate(self) -> str:
-        return 
+    def recover(self) -> str:
+        return _generate(self.shreddedCode)
 
+    def delete(self, nodeObj:ANode=None, nodeID:str=None) -> ANode:
 
+        if nodeObj:
+            if self.root != nodeObj.root:
+                raise Exception("Tree objects are not identical! Check the arguments.")
+        elif nodeID:
+            nodeObj = self.searchNodeByID(id=nodeID)
+
+        nodeObj.parent = None
+
+        targetPosStart = nodeObj.start_point
+        targetPosEnd = nodeObj.end_point
+
+        self.shreddedCode = _textDelete(splittedStrings=self.shreddedCode, 
+                                        posStart=targetPosStart, 
+                                        posEnd=targetPosEnd)
+
+        self.editedCode = _generate(self.shreddedCode)
+        self.is_edited = True
+
+        return (self, nodeObj)
+
+    def searchNodeByID(self, id:str) -> tuple:
+        return find_by_attr(node=self.root, value=id)
+
+    def searchNodeByType(self, type:str) -> tuple:
+        return findall_by_attr(node=self.root, value=type, name="type")
 
 
 def _splitText(text:str) -> list:
