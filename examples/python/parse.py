e309a613e3b609341c69b5de7153a80a3a2d5044
xwasoux <oxwasouxo@gmail.com>
2024-01-30 03:17:48 +0900

    feat: supports changes by commit a5ee275870f1ec467999e8e241c2ea81ed0ef377

--- examples/python/parse.py
+++ examples/python/parse.py
@@ -5,7 +5,10 @@ def main():
     with open(path.join(path.dirname(__file__), "input", "py_lang.py")) as f:
         code = f.read()
 
-    tree = AParser.parse(text=code, lang="python")
+    py_parser = AParser(lang="python")
+    ## If you want to build the parser to your local directory, use the following code
+    # py_parser = AParser(lang="python", build_to=os.path.join(os.path.dirname(__file__)))
+    tree = py_parser.parse(text=code)
     print(tree)
 
     return None
